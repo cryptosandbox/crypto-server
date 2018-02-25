ad392b78928bd38ccc2862f2f53172a22a9a5c2f
DrCalx <matthew.duguay@gmail.com>
2018-02-25 13:18:25 -0500

    Currencies

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const cors = require('cors')
 const cryptoDataRouter = require('./src/api/crypto-data/router')
 const transactionLogRouter = require('./src/api/transaction-log/router')
 const walletRouter = require('./src/api/wallet/router')
+const currencyRouter = require('./src/api/currencies/router')
 
 dotEnv.config()
 
@@ -17,6 +18,7 @@ app.use(bodyParser.json())
 app.use('/api/crypto-data', cryptoDataRouter)
 app.use('/api/transaction-log', transactionLogRouter)
 app.use('/api/wallet', walletRouter)
+app.use('/api/currencies', currencyRouter)
 
 app.listen(process.env.PORT || 8080, () => {
   console.log(`running on ${process.env.HOST}:${process.env.PORT}`)
--- src/controllers/poloniex.js
+++ src/controllers/poloniex.js
@@ -1,21 +1,39 @@
 const Poloniex = require('poloniex-api-node')
 let poloniex = new Poloniex()
+const _ = require('lodash')
 
 module.exports = {
   getTickers: (baseCurrency) => {
     return new Promise((resolve, reject) => {
       poloniex.returnTicker()
-      .then((ticker) => {
-        let keys = Object.keys(ticker).filter(key => key.startsWith('USDT_'))
+      .then((tickers) => {        
+        let keys = Object.keys(tickers).filter(key => key.startsWith('USDT_'))
         let filteredTickers = []
-        keys.forEach(key => filteredTickers.push({
+        _.forEach(keys, key => filteredTickers.push({
           coin: key.slice(baseCurrency.length+1),
-          last: ticker[key].last}))
+          last: tickers[key].last
+          }))
+          
         resolve(filteredTickers)        
       })
       .catch((reason) => {
         reject(reason)
       })
+    })
+  },
+
+  getCurrencies: () => {
+    return new Promise((resolve, reject) => {
+      poloniex.returnCurrencies()
+        .then(currencies => {
+          let keys = Object.keys(currencies)
+          _.forEach(keys, key => currencies[key].coin = key)
+          let filteredCurrencies = _.filter(currencies, { 'delisted': 0, 'disabled': 0, 'frozen': 0})
+          resolve(filteredCurrencies) 
+        })        
+        .catch(reason => {
+          reject(reason)
+        })
     })
   }
 }
