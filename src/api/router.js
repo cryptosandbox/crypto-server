907a0e1056636f127a28ab665b1b15e9c19d5593
DrCalx <matthew.duguay@gmail.com>
2018-03-13 13:22:08 -0400

    Get rid of 0x refs

--- src/api/router.js
+++ src/api/router.js
@@ -4,7 +4,6 @@ const router = express.Router()
 const walletRouter = require('./wallet.router')
 const poloniexController = require('../controllers/crypto/poloniex.controller')
 const walletController = require('../controllers/wallet/wallet.controller')
-const zeroExController = require('../controllers/0x/0x.controller')
 
 router.route('/transaction-log')
   .get((req, res) => {
@@ -19,16 +18,6 @@ router.route('/currencies')
 router.route('/crypto')
   .get((req, res) => {
     handleController(poloniexController.getCombined('USDT'), res)
-  })
-
-router.route('/0x')
-  .get((req, res) => {
-    handleController(zeroExController.getBalance('',''))
-  })
-
-router.route('/0x/salt')
-  .get((req, res) => {
-    handleController(zeroExController.getSalt())
   })
 
 async function handleController(controllerPromise, res) {
